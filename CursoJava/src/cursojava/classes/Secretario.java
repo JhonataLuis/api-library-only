69efabf35bb35034bfb6cf6f32e87d13ee18a381
jhona <jhona@DESKTOP-QP1RD1B>
2025-02-13 16:01:01 -0300

    Subindo códigos

--- CursoJava/src/cursojava/classes/Secretario.java
+++ CursoJava/src/cursojava/classes/Secretario.java
@@ -1,11 +1,24 @@
 package cursojava.classes;
 
-public class Secretario extends Pessoa{
+import cursojava.interfaces.PermitirAcesso;
+
+public class Secretario extends Pessoa implements PermitirAcesso{
 
 	private String registro;
 	private String nivelCargo;
 	private String experiencia;
 	
+	private String login;
+	private String senha;
+	
+	public Secretario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public Secretario() {
+		
+	}
 	
 	public String getRegistro() {
 		return registro;
@@ -37,5 +50,17 @@ public class Secretario extends Pessoa{
 		return 1800.80 * 0.9;
 	}
 	
+	
+	
+	@Override
+	public boolean autenticar(String login, String senha) {
+		
+		return autenticar();
+	}
+	@Override
+	public boolean autenticar() {
+		
+		return login.equals("admin") && senha.equals("admin");
+	}
 	
 }
