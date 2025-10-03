3236242b6dc378ecd3a0d30be1485a979d5e9f3e
Jhonata <jhonatafontesluis23@gmail.com>
2025-10-03 15:06:42 -0300

    created camada Frontend com REACT para consumir API
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/com/bmt/library_only/controller/UserController.java
+++ src/main/java/com/bmt/library_only/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class UserController {
     public ResponseEntity<List<Usuario>> getAllUsers() {
         List<Usuario> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> createUser(@RequestBody Usuario user) {
+        Usuario savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
     }
 
 }
