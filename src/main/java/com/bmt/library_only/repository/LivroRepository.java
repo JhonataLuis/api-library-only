4e237024b7d15de6dfad34f337138363a4703ff7
Jhonata <jhonatafontesluis23@gmail.com>
2025-10-04 12:53:34 -0300

    restart project api library only
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/com/bmt/library_only/repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bmt.library_only.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bmt.library_only.model.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
-}
