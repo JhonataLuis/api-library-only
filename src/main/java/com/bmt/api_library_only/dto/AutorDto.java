98aa5634ccdf611deaabae38bf4b6474073c9f4e
Jhonata <jhonatafontesluis23@gmail.com>
2025-11-04 11:18:25 -0300

    Criado dto autor
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

---
+++ src/main/java/com/bmt/api_library_only/dto/AutorDto.java
@@ -0,0 +1,76 @@
+package com.bmt.api_library_only.dto;
+
+public class AutorDto {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String nacionalidade;
+    private String biografia;
+    //private List<Livros> livros;
+
+    // Construtor
+    public AutorDto(){
+
+    }
+
+    public AutorDto(Long id, String nome, String email, String telefone, String nacionalidade, String biografia){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.nacionalidade = nacionalidade;
+        this.biografia = biografia;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    
+}
